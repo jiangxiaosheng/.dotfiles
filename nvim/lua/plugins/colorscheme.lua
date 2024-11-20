6cc7066ab2d8eb9a7f4d4dc31922adf2887b3016
jiangxiaosheng <shengjiang@cmu.edu>
2024-11-20 14:50:16 -0500

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,45 +1,90 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    local transparent = false -- set to true if you would like to enable transparency
+   'Mofiqul/vscode.nvim',
 
-    local bg = "#011628"
-    local bg_dark = "#011423"
-    local bg_highlight = "#143652"
-    local bg_search = "#0A64AC"
-    local bg_visual = "#275378"
-    local fg = "#CBE0F0"
-    local fg_dark = "#B4D0E9"
-    local fg_gutter = "#627E97"
-    local border = "#547998"
+   -- optionally, override the default options:
+    config = function()
+      local c = require('vscode.colors').get_colors()
+      require('vscode').setup({
+        -- Alternatively set style in setup
+        -- style = 'light'
 
-    require("tokyonight").setup({
-      style = "night",
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and "transparent" or "dark",
-        floats = transparent and "transparent" or "dark",
-      },
-      on_colors = function(colors)
-        colors.bg = bg
-        colors.bg_dark = transparent and colors.none or bg_dark
-        colors.bg_float = transparent and colors.none or bg_dark
-        colors.bg_highlight = bg_highlight
-        colors.bg_popup = bg_dark
-        colors.bg_search = bg_search
-        colors.bg_sidebar = transparent and colors.none or bg_dark
-        colors.bg_statusline = transparent and colors.none or bg_dark
-        colors.bg_visual = bg_visual
-        colors.border = border
-        colors.fg = fg
-        colors.fg_dark = fg_dark
-        colors.fg_float = fg
-        colors.fg_gutter = fg_gutter
-        colors.fg_sidebar = fg_dark
-      end,
+        -- Enable transparent background
+        transparent = false,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        color_overrides = {
+            vscLineNumber = '#FFFFFF',
+        },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+            -- this supports the same val table as vim.api.nvim_set_hl
+            -- use colors from this colorscheme by requiring vscode.colors!
+           Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+        }
     })
-
-    vim.cmd("colorscheme tokyonight")
-  end,
+   require('vscode').load()
+   end,
 }
+
+-- return {
+--   "folke/tokyonight.nvim",
+--   config = function()
+--     require("tokyonight").setup({
+--       style="night",
+--     })
+--     vim.cmd("colorscheme tokyonight")
+--   end,
+-- }
+
+-- return {
+--   "folke/tokyonight.nvim",
+--   priority = 1000,
+--   config = function()
+--     local transparent = false -- set to true if you would like to enable transparency
+--
+--     local bg = "#011628"
+--     local bg_dark = "#011423"
+--     local bg_highlight = "#143652"
+--     local bg_search = "#0A64AC"
+--     local bg_visual = "#275378"
+--     local fg = "#CBE0F0"
+--     local fg_dark = "#B4D0E9"
+--     local fg_gutter = "#627E97"
+--     local border = "#547998"
+--
+--     require("tokyonight").setup({
+--       style = "night",
+--       transparent = transparent,
+--       styles = {
+--         sidebars = transparent and "transparent" or "dark",
+--         floats = transparent and "transparent" or "dark",
+--       },
+--       on_colors = function(colors)
+--         colors.bg = bg
+--         colors.bg_dark = transparent and colors.none or bg_dark
+--         colors.bg_float = transparent and colors.none or bg_dark
+--         colors.bg_highlight = bg_highlight
+--         colors.bg_popup = bg_dark
+--         colors.bg_search = bg_search
+--         colors.bg_sidebar = transparent and colors.none or bg_dark
+--         colors.bg_statusline = transparent and colors.none or bg_dark
+--         colors.bg_visual = bg_visual
+--         colors.border = border
+--         colors.fg = fg
+--         colors.fg_dark = fg_dark
+--         colors.fg_float = fg
+--         colors.fg_gutter = fg_gutter
+--         colors.fg_sidebar = fg_dark
+--       end,
+--     })
+--
+--     vim.cmd("colorscheme tokyonight")
+--   end,
+-- }
